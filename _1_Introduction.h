721debb0d5310ea01e12c2b1c80a7d3027fca330
Mehrdad Solimanimajd <s.mehrdad.47@outlook.com>
2018-12-20 11:23:06 +0330

    SomeChanges+MakingReadyForAPossiblyNewStructure

---
+++ _1_Introduction.h
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------
+/// <summary>
+/// _1_Introduction.h
+/// </summary>
+/// <created>ʆϒʅ,20.12.2018</created>
+/// <changed>ʆϒʅ,20.12.2018</changed>
+// --------------------------------------------------------------------------------
+
+#pragma once
+
+
+#ifndef _1_INTRODUCTION_H
+#define _1_INTRODUCTION_H
+
+
+void Compilers ();
+
+
+#endif // !_1_INTRODUCTION_H
---
+++ _2_BasicsOfCplusPlus.h
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------
+/// <summary>
+/// _2_Basics.h
+/// </summary>
+/// <created>ʆϒʅ,11.04.2018</created>
+/// <changed>ʆϒʅ,20.12.2018</changed>
+// --------------------------------------------------------------------------------
+
+#pragma once
+
+
+#ifndef INTRODUCTIONANDBASICS_H
+#define INTRODUCTIONANDBASICS_H
+
+
+void HelloWorld ();
+void VariablesAndTypes ();
+void Constants ();
+void Operators ();
+void BasicInputOutput ();
+
+
+#endif // !INTRODUCTIONANDBASICS_H
